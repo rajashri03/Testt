ac2a8e8f2cd753903b11889c5b0d7083895b4b7c
rajashri03 <telvekar.rajashri@gmail.com>
2023-06-02 09:39:38 +0530

    Adiition code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        //uc1-addition
+        //uc2-sub
+        //uc3-div
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adiition");
+        }
+    }
+}
